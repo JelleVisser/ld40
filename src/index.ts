e6bedf1014ba013f070f34139af887d8120c2d8a
Eric <eric@obi4wan.nl>
2017-12-02 11:29:29 +0100

    Update vscode settings regarding formatting.

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,38 @@ import * as Phaser from 'phaser';
 import Config from './config';
 
 class SimpleGame {
-  game: Phaser.Game;
-  logo: Phaser.Sprite;
-  cursors: Phaser.CursorKeys;
+    game: Phaser.Game;
+    logo: Phaser.Sprite;
+    cursors: Phaser.CursorKeys;
 
-  constructor() {
-    this.game = new Phaser.Game(Config.width, Config.height, Phaser.AUTO, "content", this);
-  }
+    constructor() {
+        this.game = new Phaser.Game(Config.width, Config.height, Phaser.AUTO, "content", this);
+    }
 
-  preload() {
-    this.game.load.image("logo", "./assets/images/mushroom2.png");
-  }
+    preload() {
+        this.game.load.image("logo", "./assets/images/mushroom2.png");
+    }
 
-  create() {
-    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-    this.logo.anchor.setTo(0.5, 0.5);
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-  }
+    create() {
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
+        this.logo.anchor.setTo(0.5, 0.5);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
 
-  update() {
-    this.game.input.update();
+    update() {
+        this.game.input.update();
 
-    if (this.cursors.down.isDown)
-      this.logo.position.y += 10;
-    if (this.cursors.up.isDown)
-      this.logo.position.y -= 10;
-    if (this.cursors.left.isDown)
-      this.logo.position.x -= 10;
-    if (this.cursors.right.isDown)
-      this.logo.position.x += 10;
-  }
+        if (this.cursors.down.isDown)
+            this.logo.position.y += 10;
+        if (this.cursors.up.isDown)
+            this.logo.position.y -= 10;
+        if (this.cursors.left.isDown)
+            this.logo.position.x -= 10;
+        if (this.cursors.right.isDown)
+            this.logo.position.x += 10;
+    }
 }
 
 window.onload = () => {
-  const game = new SimpleGame();
+    const game = new SimpleGame();
 };
